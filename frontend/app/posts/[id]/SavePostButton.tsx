b00f41f4925aea4ff86b9b449037399c500b0bcf
dariakulish1 <dashaklsh1507@gmail.com>
2026-09-11 15:12:19 +0300

    small icon fix

--- frontend/app/posts/[id]/SavePostButton.tsx
+++ frontend/app/posts/[id]/SavePostButton.tsx
@@ -22,7 +22,7 @@ export function SavePostButton({ postId }: { postId: string }) {
       onClick={handleToggleSave}
       className="flex flex-row gap-4 items-center border border-gray-200 py-2 px-4 h-10 rounded-full cursor-pointer hover:bg-gray-100 transition-all duration-200"
     >
-      {saved ? <BookmarkCheck className="w-6 h-6 text-gray-700" /> : <Bookmark className="w-6 h-6 text-gray-500" />}
+      {saved ? <BookmarkCheck className="w-5 h-5 text-gray-700" /> : <Bookmark className="w-5 h-5 text-gray-500" />}
       <p>{saved ? 'Saved' : 'Save'}</p>
     </button>
   );
